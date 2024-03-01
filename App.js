9c51f5cc04770ced32526ca104a416d311797ddf
unknown <andreyandreydn16@gmail.com>
2024-03-01 20:42:22 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Shitting } from "./components/Shitting";
+
+function App() {
+  return (
+    <div className="App">
+      тут мое приложение и оно работает.
+      <Shitting />
+    </div>
+  );
+}
+
+export default App;
